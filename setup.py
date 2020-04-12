5e4920c1fd76458f376d94c5225e39f470294c9a
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-04-12 10:54:16 +0300

    Add stuff.

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ import sys
 CURRENT_PYTHON = sys.version_info[:2]
 REQUIRED_PYTHON = (3, 6)
 
-# This check and everything above must remain compatible with Python 2.7.
 if CURRENT_PYTHON < REQUIRED_PYTHON:
     sys.stderr.write("""
-""")
+==========================
+Unsupported Python version
+==========================
+
+This version requires Python {}.{}, but you're trying to
+install it on Python {}.{}.
+""".format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
     sys.exit(1)
 
 setup()
