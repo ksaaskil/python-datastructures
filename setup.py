3721b5b5286b6187af02ab6e9ec77426192cd3a2
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-04-12 10:46:11 +0300

    First commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import sys
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 6)
+
+# This check and everything above must remain compatible with Python 2.7.
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write("""
+""")
+    sys.exit(1)
+
+setup()
